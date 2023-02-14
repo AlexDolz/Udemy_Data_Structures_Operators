9d7dbaea1cc6125038f97dd42a391ab89281b685
AlexDolz <aleksejdolznikov3@gmail.com>
2023-02-14 17:20:00 +0100

    updated data structures

--- script.js
+++ script.js
@@ -504,3 +504,77 @@ console.log(
 ); // 3
 
 console.log(new Set('jonasschmedtmann').size);
+
+// *********************** Maps fundamental ***************************
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :)')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+// rest.clear();
+
+const arr3 = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest.size);
+rest.clear(rest.size);
+// rest.set(document.querySelector('h1'), 'Heading');
+
+console.log(rest.get([1, 2]));
+
+// console.log(rest.get(arr));
+
+// ************************ Maps iterations **************************
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct🎀'],
+  [false, 'Try again!'],
+]);
+
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key} : ${value}`);
+  }
+}
+
+const answer = Number(prompt('Your naswer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
